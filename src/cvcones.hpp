41be9c441d8ae396adf3b567caa6e66a7b389d26
marbae <martin9421@gmail.com>
2018-11-22 18:52:11 +0100

    Update cvcones.hpp

--- src/cvcones.hpp
+++ src/cvcones.hpp
@@ -17,6 +17,10 @@
  * USA.
  */
 
+/*CVcones is an object containing the cones sent from detectcone
+at the moment it just used as stopgap if the localization fails so that the car can get local information
+in the second lap. If the localization improves and information can be sent from detectcone there is no need for this class*/
+
 #ifndef CVCONES_HPP
 #define CVCONES_HPP
 
@@ -46,10 +50,10 @@ class CVCones{
   Eigen::Vector3d Spherical2Cartesian(double azimuth, double zenimuth, double distance);
   Eigen::Vector2d transformConeToCoG(double angle, double distance);
 
-  std::vector<Cone> m_cones = {};
-  cluon::data::TimeStamp m_lastTimeStamp = {};
-  std::mutex m_coneMutex;
-  std::mutex m_stampMutex;
+  std::vector<Cone> m_cones = {}; //Vector of cones received from detectcone
+  cluon::data::TimeStamp m_lastTimeStamp = {}; //Timestamp of the received cones used for comparison
+  std::mutex m_coneMutex; //Mutex for the access of the information in this class
+  std::mutex m_stampMutex; //Mutex for altering timestamp
   // Constants for degree transformation
   const double DEG2RAD = 0.017453292522222; // PI/180.0
   const double RAD2DEG = 57.295779513082325; // 1.0 / DEG2RAD;
